978d2e31a5273d1d48b0f770d647983533c7aab0
lamakais04-dot <lama.kais04@gmail.com>
2026-01-27 15:08:34 +0200

    added authintication with navbar

--- Frontend/src/components/layouts/layout/layout.jsx
+++ Frontend/src/components/layouts/layout/layout.jsx
@@ -1,27 +1,25 @@
 import React from 'react'
 import AllBooks from '../../pages/books'
-import { Routes, Route, Outlet } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Navbar from '../header/navbar'
 import '../../csspages/layout.css'
 import Login from '../../pages/login'
 import HomePage from '../../pages/homePage'
 import Signup from '../../pages/signup'
 
-
 export default function Layout() {
     return (
         <>
             <div className='layout'>
-                <Navbar></Navbar>
+                <Navbar />
                 <div id="component">
-                <Routes>
-                    <Route path='/book' Component={AllBooks} />
-                    <Route path='/login' Component={Login}/>
-                    <Route path='/' Component={HomePage}/>
-                    <Route path='/signup' Component={Signup}/>
-                </Routes>
+                    <Routes>
+                        <Route path='/' element={<HomePage />} />
+                        <Route path='/book' element={<AllBooks />} />
+                        <Route path='/login' element={<Login />} />
+                        <Route path='/signup' element={<Signup />} />
+                    </Routes>
                 </div>
-                <Outlet />
             </div>
         </>
     )
